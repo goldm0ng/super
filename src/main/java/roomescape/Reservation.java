a7e7b1cfc29d81fc509a2d4fa9fab548e6ee38c0
goldm0ng <jujs45177@gmail.com>
2024-11-07 00:02:17 +0900

    <Add> 핵심 도메인 Reservation 추가

---
+++ src/main/java/roomescape/Reservation.java
@@ -0,0 +1,23 @@
+package roomescape;
+
+import lombok.Data;
+
+@Data
+public class Reservation {
+
+    private Long id;
+    private String name;
+    private String date;
+    private String time;
+
+    public Reservation() {
+    }
+
+    public Reservation(Long id, String name, String date, String time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+}
