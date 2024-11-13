2bf90d0f8ea8bf5ce3ae3e0547e3164e6b25cbbe
goldm0ng <jujs45177@gmail.com>
2024-11-13 16:30:47 +0900

    <ADD> 예약 비즈니스 관련 Service 추가

---
+++ src/main/java/roomescape/service/ReservationService.java
@@ -0,0 +1,30 @@
+package roomescape.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import roomescape.domain.Reservation;
+import roomescape.dto.ReservationDto;
+import roomescape.repository.ReservationRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ReservationService {
+
+    private final ReservationRepository repository;
+
+    public Reservation addReservation(ReservationDto reservationDto){
+        return repository.save(reservationDto);
+    }
+
+    public List<Reservation> checkReservations(){
+        return repository.findAll();
+    }
+
+    public Optional<Reservation> deleteReservation(Long reservationId){
+        return repository.delete(reservationId);
+    }
+
+}
